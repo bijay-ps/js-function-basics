e180d7682893f39973c7d4b6489282dbed00aeec
bijay_ps <bijay.prakash7@yahoo.in>
2023-08-22 22:22:45 +0530

    Class exercise completed

--- classExercise.js
+++ classExercise.js
@@ -15,27 +15,23 @@ const fname = getFullName("Bijay", "Singh");
 // console.log(fname);
 
 function isPrime(num) {
-    console.log(num)
     let isPrime = true;
     if(num === 1) {
         // return; simply stops the function execution and return 
         return `${num} is not a prime number`
     }
 
-    while( num > 1) {
-        for (let i = 2; i < num; i++) {
-            console.log(num%i)
-            if(num % i === 0) {
-                isPrime = false;
-                break;
-            } 
-        }
+    for (let i = 2; i < num; i++) {
+        if(num % i === 0) {
+            isPrime = false;
+            break;
+        } 
     }
     return isPrime;
 }
 
 const res = isPrime(7);
-console.log(res);
+console.log(`Is 7 a prime number: ${res}`);
 
 const res2 = isPrime(10);
-console.log(res2)
+console.log(`Is 10 a prime number: ${res2}`);
